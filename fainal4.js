74275130fcf15c21c5a5368b0af30ca8de1a861b
user45126 <91059141+user45126@users.noreply.github.com>
2021-12-25 10:45:21 +0600

    Add files via upload

---
+++ fainal4.js
@@ -0,0 +1,24 @@
+function change2(){
+    document.getElementById("0").style.display="none";
+    document.getElementById("2").style.display="block";
+    document.getElementById("3").style.display="none";
+    document.getElementById("but2").style.backgroundColor="#f3df55";
+      document.getElementById("but1").style.backgroundColor="white";
+      document.getElementById("but3").style.backgroundColor="white";
+  }
+  function change1(){
+      document.getElementById("0").style.display="block";
+      document.getElementById("2").style.display="none";
+      document.getElementById("3").style.display="none";
+      document.getElementById("but1").style.backgroundColor="#f3df55";
+      document.getElementById("but2").style.backgroundColor="white";
+      document.getElementById("but3").style.backgroundColor="white";
+  }
+  function change3(){
+      document.getElementById("0").style.display="none";
+      document.getElementById("2").style.display="none";
+      document.getElementById("3").style.display="block";
+      document.getElementById("but1").style.backgroundColor="white";
+      document.getElementById("but2").style.backgroundColor="white";
+      document.getElementById("but3").style.backgroundColor="#f3df55";
+  }
